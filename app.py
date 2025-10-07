a9a051cedfc32e2994ebe4ef05763111e51fed65
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-10-07 17:49:59 +0530

    Fix syntax for accessing GCP service account secrets

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ SHEET_ID = "1dlPKBqb6fNB5xp2l0uqXSW4WSmw-_hhSjJNx3CrGJnU"
 
 # Connect to Google Sheet
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-creds_dict = **st.secrets.gcp_service_account"
+creds_dict = **st.secrets.gcp_service_account
 #st.write(creds_dict)
 if "data" not in st.session_state:
     creds = Credentials.from_service_account_info(creds_dict)
